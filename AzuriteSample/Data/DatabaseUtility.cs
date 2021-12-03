74faa38d11b641c9e8fabbefc30dab7ec4f2ea6f
Vijay Ramakrishnan <vramak@microsoft.com>
2021-12-02 17:18:55 -0800

    Apply automatic db migration for development.

---
+++ AzuriteSample/Data/DatabaseUtility.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AzuriteSample.Data
+{
+    public static class DatabaseUtility
+    {
+        /// Please don't use this for production databases. This is only intended for development to create and pre-seed the database.
+        public static async Task EnsureDbCreatedAndSeedAsync(this DbContextOptions<AzuriteSampleContext> options)
+        {
+            var builder = new DbContextOptionsBuilder<AzuriteSampleContext>(options);
+            builder.UseLoggerFactory(new LoggerFactory());
+            using var context = new AzuriteSampleContext(builder.Options);
+            if (await context.Database.EnsureCreatedAsync())
+            {
+                // Add the code to seed the database
+            }
+        }
+    }
+}
--- AzuriteSample/Program.cs
+++ AzuriteSample/Program.cs
@@ -17,8 +17,13 @@ builder.Services.AddAzureClients(clientBuilder =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    await using var scope = app.Services.GetService<IServiceScopeFactory>().CreateAsyncScope();
+    var options = scope.ServiceProvider.GetRequiredService<DbContextOptions<AzuriteSampleContext>>();
+    await DatabaseUtility.EnsureDbCreatedAndSeedAsync(options);
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
